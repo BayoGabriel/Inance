10c14ff179198d938265050c51ffe142dc947745
Bayode Gabriel <Bayogabriel24@gmail.com>
2023-10-03 18:58:01 +0100

    v1.0

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "../Assets/styles/Navbar.css"
+const Navbar = () => {
+  return (
+    <div>
+       <nav className="nav">
+        <div className="logo">
+            INANCE
+        </div>
+        <div className="links">
+            <ul id='navbarr'>
+                <li><a className='active' href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </div>
+        <div className="mobile">
+            <i class="fas fa-bars"></i>
+            <i className='fas fa-times'></i>
+        </div>
+       </nav>
+    </div>
+  )
+}
+
+export default Navbar
